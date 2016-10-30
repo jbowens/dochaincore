43c99224ed7fd6bd2e7b7d741e50693ab907b2d8
Jackson Owens <jackson@chain.com>
2016-10-29 17:49:13 -0700

    no tty

--- userdata.go
+++ userdata.go
@@ -16,8 +16,8 @@ runcmd:
   - mkfs.ext4 -F /dev/disk/by-id/scsi-0DO_Volume_chain-core-storage
   - mkdir -p /mnt/chain-core-storage
   - mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_chain-core-storage /mnt/chain-core-storage
-  - echo '/dev/disk/by-id/scsi-0DO_Volume_chain-core-storage /mnt/chain-core-storage ext4 defaults,nofail,discard 0 0' | tee -a /etc/fstab
-  - docker run -it -p 1999:1999 -v /mnt/chain-core-storage/postgresql/data:/var/lib/postgresql/data chaincore/developer
+  - echo '/dev/disk/by-id/scsi-0DO_Volume_chain-core-storage /mnt/chain-core-storage ext4 defaults,nofail,discard 0 0' >> /etc/fstab
+  - docker run -p 1999:1999 -v /mnt/chain-core-storage/postgresql/data:/var/lib/postgresql/data chaincore/developer
 `
 
 func buildUserData(opt *options) (string, error) {
